0a447dbcef096515fe7cae4b3e891e37d675e7e8
kevins4202 <241songk@student.dist113.org>
2022-07-25 18:57:33 -0500

    reorganized files

--- code/replace_bad_data.py
+++ code/replace_bad_data.py
@@ -2,15 +2,17 @@ from cmath import isnan
 import os
 from random import random
 import time
+from typing import Any
 import pandas as pd
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
+from csv import writer
 
 # read the data from a file
-data = pd.read_csv(os.getcwd().replace('code/','') + "/data/cleaned_prof_data_v4.csv")
+data = pd.read_csv(os.getcwd() + "/data/cleaned_prof_data_v4.csv")
 
 # initialize web driver for selenium
 options = webdriver.ChromeOptions()
@@ -36,17 +38,14 @@ def findMaxCitations(web_elements: list):
 professors = []
 professorExists = True
 
-for i in range(12000, 15000):
-
+for i in range(400, 801):
+    
     # initialize values
-    scholar_id = data.iat[i, 3],
-    citations = data.iat[i, 4],
+    scholar_id = data.iat[i, 3]
+    citation = data.iat[i, 4]
     h_index = data.iat[i, 5]
 
-    if(data.iat[i, 3] == "NOSCHOLARPAGE" or data.iat[i, 3] == "#NAME?" or isnan(data.iat[i, 4]) or isnan(data.iat[i, 5])):
-        print("Name: " + data.iat[i, 0] + " | " + "University: " +
-              data.iat[i, 1] + " | " + "id: " + data.iat[i, 3])
-
+    if (data.iat[i, 3] == "NOSCHOLARPAGE" or data.iat[i, 3] == "#NAME?" or isnan(data.iat[i, 4]) or isnan(data.iat[i, 5])):
         # launch URL
         driver.get("https://scholar.google.com")
 
@@ -71,7 +70,6 @@ for i in range(12000, 15000):
                 # in the case there are multiple options in the recommended table
                 list_of_elements = driver.find_element(
                     By.XPATH, "//*[@id='gs_res_ccl_mid']/div[1]/table/tbody/tr/td[2]").find_elements(By.TAG_NAME, "div")
-                print("case 2 was executed")
                 max_element = findMaxCitations(list_of_elements)
                 max_element.find_element(By.TAG_NAME, "a").click()
                 time.sleep(random() * 5)
@@ -79,7 +77,6 @@ for i in range(12000, 15000):
                 # otherwise loop through all the divs and find the correct professor
                 elements_list = driver.find_element(
                     By.XPATH, "//*[@id='gs_res_ccl_mid']/div[1]").find_element(By.CLASS_NAME, "gs_a").find_elements(By.TAG_NAME, "a")
-                print(elements_list)
                 if elements_list: 
                     for element in elements_list:  # looping through the number of professors in the link
                         try:
@@ -98,29 +95,28 @@ for i in range(12000, 15000):
         # retrieve citation, h-index, and scholar_id for that prof
         if professorExists:
             scholar_id = driver.current_url.split("=")[1].split("&")[0]
-            citation = driver.find_element(
-                By.XPATH, "//*[@id='gsc_rsb_st']/tbody/tr[1]/td[2]").text
-            h_index = driver.find_element(
-                By.XPATH, "//*[@id='gsc_rsb_st']/tbody/tr[2]/td[2]").text
+            citation = int(driver.find_element(
+                By.XPATH, "//*[@id='gsc_rsb_st']/tbody/tr[1]/td[2]").text)
+            h_index = int(driver.find_element(
+                By.XPATH, "//*[@id='gsc_rsb_st']/tbody/tr[2]/td[2]").text)
             time.sleep(random() * 5)
         else:
             scholar_id = None
             citation = None
             h_index = None
-        print("scholar_id: " + str(scholar_id) + " h-index: " +
-              str(h_index) + " citations: " + str(citation))
-        professors.append({
-            "Name": data.iat[i, 0],
-            "affiliation": data.iat[i, 1],
-            "homepage": data.iat[i, 2],
-            "scholar-id": scholar_id,
-            "citations": citation,
-            "h-index": h_index
-        })
-    else:
-        continue
+    
+    print(i)
+    professor = { 
+        "Name": data.iat[i, 0],
+        "affiliation": data.iat[i, 1],
+        "homepage": data.iat[i, 2],
+        "scholar-id": scholar_id,
+        "citations": citation,
+        "h-index": h_index
+    }
+    print(professor)
+    professors.append(professor)
 
 
-print(len(professors))
 df = pd.DataFrame(professors)
-df.to_csv("cleaned_prof_data_v5_30.csv")
+df.to_csv("data/cleaned_prof_data_v5_2.csv")
